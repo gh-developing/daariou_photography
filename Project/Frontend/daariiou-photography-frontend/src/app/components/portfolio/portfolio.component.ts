2983f6147d8789eb693ef028ecc9055de666b390
Shachar Alon <shachar.alon@bystronic.com>
2021-11-25 15:02:45 +0100

    i swear to god I hate bootstrap modals didnt allow me to parse FCKN ANGULAR DATA in it and had to write EACH FCKN PICTURE by hand in the nxt version we just using angular dialogs istg

--- Project/Frontend/daariiou-photography-frontend/src/app/components/portfolio/portfolio.component.ts
+++ Project/Frontend/daariiou-photography-frontend/src/app/components/portfolio/portfolio.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  public five = "#five"
+
+  public gallery1 = [
+    { id: "1", picture: "assets/20210421_065614.jpg" },
+    { id: "2", picture: "assets/DSC00150.jpg" },
+    { id: "3", picture: "assets/DSC00302.jpg" },
+    { id: "4", picture: "assets/DSC00370.jpg" },
+    { id: "5", picture: "assets/DSC00993.jpg" },
+    { id: "6", picture: "assets/DSC00845-3.jpg" },
+    { id: "7", picture: "assets/DSC00928-3.jpg" },
+    { id: "8", picture: "assets/DSC07337.jpg" },
+    { id: "9", picture: "assets/DSC07491-2.jpg" },
+    { id: "0", picture: "assets/DSC07609-2.jpg" },
+    { id: "1", picture: "assets/DSC08046.jpg" },
+    { id: "2", picture: "assets/DSC08825.jpg" },
+    { id: "3", picture: "assets/DSC08901.jpg" },
+    { id: "4", picture: "assets/DSC09047.jpg" },
+    { id: "5", picture: "assets/DSC09289.jpg" },
+    { id: "6", picture: "assets/DSC09592.jpg" },
+    { id: "1", picture: "assets/DSC09864.jpg" },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.gallery1[1].id)
   }
 
 }
