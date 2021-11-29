b6e287e089a3e1c3623249b5bfea05a343c0c50e
Shachar Alon <shachar.alon@bystronic.com>
2021-11-29 12:01:03 +0100

    oh no its the password hashshshshshshshshshshshshshshshsh snake

---
+++ Project/Backend/daariiou_photography_backend/daariiou_photography_backend/Helper/PasswordHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace daariiou_photography_backend.Helper
+{
+    public class PasswordHelper
+    {
+        /// <summary>
+        /// Gets the hash string.
+        /// Creates the password hash with Salt of choice.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        private static string CreatePasswordHash(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(password))
+                sb.Append(b.ToString("X2"));
+            string hashedPW = sb.ToString();
+            return hashedPW;
+        }
+
+        /// <summary>
+        /// Gets the hash.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        private static byte[] GetHash(string password)
+        {
+            SHA384 sha = new SHA384CryptoServiceProvider();
+            return sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+    }
+
+}
