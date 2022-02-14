a803d39adfa5be528b19736ea1f0058fe8470bed
Shachar Alon <shachar.alon@bystronic.com>
2022-02-14 09:41:08 +0100

    he wanted that i added pctures brooo thisshit is some gb big its loading like ur mom when shes trying to think.

--- Project/Frontend/daariiou-photography-frontend/src/assets/pictures.ts
+++ Project/Frontend/daariiou-photography-frontend/src/assets/pictures.ts
@@ -6,291 +6,499 @@ export class PictureList {
     public pictures = [
         {
             id: '1',
-            src: 'assets/20210421_065614.jpg',
-            thumb: 'assets/20210421_065614.jpg',
+            src: 'assets/img/20210421_065614.jpg',
+            thumb: 'assets/img/20210421_065614.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 3
         },
         {
             id: '2',
-            src: 'assets/DSC00150.jpg',
-            thumb: 'assets/DSC00150.jpg',
+            src: 'assets/img/DSC00150.jpg',
+            thumb: 'assets/img/DSC00150.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 3,
         },
         {
             id: '3',
-            src: 'assets/DSC00302.jpg',
-            thumb: 'assets/DSC00302.jpg',
+            src: 'assets/img/DSC00302.jpg',
+            thumb: 'assets/img/DSC00302.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '4',
-            src: 'assets/DSC00370.jpg',
-            thumb: 'assets/DSC00370.jpg',
+            src: 'assets/img/DSC00370.jpg',
+            thumb: 'assets/img/DSC00370.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '5',
-            src: 'assets/DSC00993.jpg',
-            thumb: 'assets/DSC00993.jpg',
-            format: 1,
+            src: 'assets/img/DSC00993.jpg',
+            thumb: 'assets/img/DSC00993.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 3
         },
         {
             id: '6',
-            src: 'assets/DSC00845-3.jpg',
-            thumb: 'assets/DSC00845-3.jpg',
-            format: 1,
+            src: 'assets/img/DSC00845-3.jpg',
+            thumb: 'assets/img/DSC00845-3.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '7',
-            src: 'assets/DSC00928-3.jpg',
-            thumb: 'assets/DSC00928-3.jpg',
-            format: 1,
-            subHtml: 'Image',
-            kindOfShooting: 2
-        },
-        {
-            id: '7.1',
-            src: 'assets/DSC01840.jpg',
-            thumb: 'assets/DSC01840.jpg',
+            src: 'assets/img/DSC00928-3.jpg',
+            thumb: 'assets/img/DSC00928-3.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '8',
-            src: 'assets/DSC01509.jpg',
-            thumb: 'assets/DSC01509.jpg',
-            format: 1,
+            src: 'assets/img/DSC01840.jpg',
+            thumb: 'assets/img/DSC01840.jpg',
+            format: 0,
             subHtml: 'Image',
-            kindOfShooting: 1
+            kindOfShooting: 2
         },
         {
             id: '9',
-            src: 'assets/DSC01543.jpg',
-            thumb: 'assets/DSC01543.jpg',
+            src: 'assets/img/DSC01509.jpg',
+            thumb: 'assets/img/DSC01509.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '10',
-            src: 'assets/DSC01579.jpg',
-            thumb: 'assets/DSC01579.jpg',
-            format: 1,
+            src: 'assets/img/DSC01543.jpg',
+            thumb: 'assets/img/DSC01543.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '11',
-            src: 'assets/DSC01642.jpg',
-            thumb: 'assets/DSC01642.jpg',
+            src: 'assets/img/DSC01579.jpg',
+            thumb: 'assets/img/DSC01579.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '12',
-            src: 'assets/DSC09390.jpg',
-            thumb: 'assets/DSC09390.jpg',
-            format: 1,
+            src: 'assets/img/DSC09390.jpg',
+            thumb: 'assets/img/DSC09390.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '13',
-            src: 'assets/DSC01755.jpg',
-            thumb: 'assets/DSC01755.jpg',
+            src: 'assets/img/DSC01755.jpg',
+            thumb: 'assets/img/DSC01755.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '14',
-            src: 'assets/DSC07337.jpg',
-            thumb: 'assets/DSC07337.jpg',
+            src: 'assets/img/DSC07337.jpg',
+            thumb: 'assets/img/DSC07337.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '15',
-            src: 'assets/DSC07491-2.jpg',
-            thumb: 'assets/DSC07491-2.jpg',
+            src: 'assets/img/DSC07491-2.jpg',
+            thumb: 'assets/img/DSC07491-2.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '16',
-            src: 'assets/DSC09871.jpg',
-            thumb: 'assets/DSC09871.jpg',
-            format: 1,
+            src: 'assets/img/DSC09871.jpg',
+            thumb: 'assets/img/DSC09871.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '17',
-            src: 'assets/DSC07609-2.jpg',
-            thumb: 'assets/DSC07609-2.jpg',
-            format: 1,
+            src: 'assets/img/DSC07609-2.jpg',
+            thumb: 'assets/img/DSC07609-2.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '18',
-            src: 'assets/DSC08046.jpg',
-            thumb: 'assets/DSC08046.jpg',
+            src: 'assets/img/DSC08046.jpg',
+            thumb: 'assets/img/DSC08046.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '19',
-            src: 'assets/DSC08825.jpg',
-            thumb: 'assets/DSC08825.jpg',
+            src: 'assets/img/DSC08825.jpg',
+            thumb: 'assets/img/DSC08825.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '20',
-            src: 'assets/DSC08901.jpg',
-            thumb: 'assets/DSC08901.jpg',
-            format: 1,
+            src: 'assets/img/DSC08901.jpg',
+            thumb: 'assets/img/DSC08901.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '21',
-            src: 'assets/DSC09047.jpg',
-            thumb: 'assets/DSC09047.jpg',
-            format: 1,
+            src: 'assets/img/DSC09047.jpg',
+            thumb: 'assets/img/DSC09047.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
             id: '22',
-            src: 'assets/DSC09289.jpg',
-            thumb: 'assets/DSC09289.jpg',
-            format: 1,
+            src: 'assets/img/DSC09289.jpg',
+            thumb: 'assets/img/DSC09289.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '23',
-            src: 'assets/DSC09592.jpg',
-            thumb: 'assets/DSC09592.jpg',
+            src: 'assets/img/DSC09592.jpg',
+            thumb: 'assets/img/DSC09592.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '24',
-            src: 'assets/DSC09392.jpg',
-            thumb: 'assets/DSC09392.jpg',
-            format: 1,
-            subHtml: 'Image',
-            kindOfShooting: 1
-        },
-        {
-            id: '24.1',
-            src: 'assets/DSC01892-6.jpg',
-            thumb: 'assets/DSC01892-6.jpg',
-            format: 1,
-            subHtml: 'Image',
-            kindOfShooting: 2
-        },
-        {
-            id: '25',
-            src: 'assets/DSC01496.jpg',
-            thumb: 'assets/DSC01496.jpg',
+            src: 'assets/img/DSC09392.jpg',
+            thumb: 'assets/img/DSC09392.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
+            id: '25',
+            src: 'assets/img/DSC01892-6.jpg',
+            thumb: 'assets/img/DSC01892-6.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
             id: '26',
-            src: 'assets/DSC09864.jpg',
-            thumb: 'assets/DSC09864.jpg',
+            src: 'assets/img/DSC01496.jpg',
+            thumb: 'assets/img/DSC01496.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '27',
-            src: 'assets/DSC08082.jpg',
-            thumb: 'assets/DSC08082.jpg',
-            format: 1,
+            src: 'assets/img/DSC09864.jpg',
+            thumb: 'assets/img/DSC09864.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '28',
-            src: 'assets/DSC08108.jpg',
-            thumb: 'assets/DSC08108.jpg',
+            src: 'assets/img/DSC08082.jpg',
+            thumb: 'assets/img/DSC08082.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '29',
-            src: 'assets/DSC08485.jpg',
-            thumb: 'assets/DSC08485.jpg',
-            format: 1,
+            src: 'assets/img/DSC08108.jpg',
+            thumb: 'assets/img/DSC08108.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '30',
-            src: 'assets/DSC08550.jpg',
-            thumb: 'assets/DSC08550.jpg',
-            format: 1,
+            src: 'assets/img/DSC08485.jpg',
+            thumb: 'assets/img/DSC08485.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '31',
-            src: 'assets/DSC09839.jpg',
-            thumb: 'assets/DSC09839.jpg',
-            format: 1,
+            src: 'assets/img/DSC08550.jpg',
+            thumb: 'assets/img/DSC08550.jpg',
+            format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '32',
-            src: 'assets/DSC01539.jpg',
-            thumb: 'assets/DSC01539.jpg',
+            src: 'assets/img/DSC09839.jpg',
+            thumb: 'assets/img/DSC09839.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 1
         },
         {
             id: '33',
-            src: 'assets/DSC01921.jpg',
-            thumb: 'assets/DSC01921.jpg',
+            src: 'assets/img/DSC01539.jpg',
+            thumb: 'assets/img/DSC01539.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '33',
+            src: 'assets/img/DSC01921.jpg',
+            thumb: 'assets/img/DSC01921.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 2
         },
         {
-            id: '33',
-            src: 'assets/DSC00166.jpg',
-            thumb: 'assets/DSC00166.jpg',
+            id: '34',
+            src: 'assets/img/DSC00166.jpg',
+            thumb: 'assets/img/DSC00166.jpg',
             format: 0,
             subHtml: 'Image',
             kindOfShooting: 3
+        },
+        {
+            id: '35',
+            src: 'assets/img/20200418_102031 (1.jpg',
+            thumb: 'assets/img/20200418_102031 (1.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '36',
+            src: 'assets/img/20211011_090158.jpg',
+            thumb: 'assets/img/20211011_090158.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2,
+        },
+        {
+            id: '37',
+            src: 'assets/img/DSC00071.jpg',
+            thumb: 'assets/img/DSC00071.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '38',
+            src: 'assets/img/DSC00091.jpg',
+            thumb: 'assets/img/DSC00091.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '39',
+            src: 'assets/img/DSC00117.jpg',
+            thumb: 'assets/img/DSC00117.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '40',
+            src: 'assets/img/DSC00239.jpg',
+            thumb: 'assets/img/DSC00239.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '41',
+            src: 'assets/img/DSC00293.jpg',
+            thumb: 'assets/img/DSC00293.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '42',
+            src: 'assets/img/DSC00319.jpg',
+            thumb: 'assets/img/DSC00319.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '43',
+            src: 'assets/img/DSC00967.jpg',
+            thumb: 'assets/img/DSC00967.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '44',
+            src: 'assets/img/DSC01665.jpg',
+            thumb: 'assets/img/DSC01665.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '45',
+            src: 'assets/img/DSC01911.jpg',
+            thumb: 'assets/img/DSC01911.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '46',
+            src: 'assets/img/DSC02835.jpg',
+            thumb: 'assets/img/DSC02835.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '47',
+            src: 'assets/img/DSC02884.jpg',
+            thumb: 'assets/img/DSC02884.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '48',
+            src: 'assets/img/DSC02903.jpg',
+            thumb: 'assets/img/DSC02903.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '49',
+            src: 'assets/img/DSC03108.jpg',
+            thumb: 'assets/img/DSC03108.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '50',
+            src: 'assets/img/DSC03118.jpg',
+            thumb: 'assets/img/DSC03118.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '51',
+            src: 'assets/img/DSC03125.jpg',
+            thumb: 'assets/img/DSC03125.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 3
+        },
+        {
+            id: '52',
+            src: 'assets/img/DSC07149 2.jpg',
+            thumb: 'assets/img/DSC07149 2.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '53',
+            src: 'assets/img/DSC07237-2.jpg',
+            thumb: 'assets/img/DSC07237-2.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '54',
+            src: 'assets/img/DSC08673.jpg',
+            thumb: 'assets/img/DSC08673.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '55',
+            src: 'assets/img/DSC08756-2.jpg',
+            thumb: 'assets/img/DSC08756-2.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '56',
+            src: 'assets/img/DSC08914.jpg',
+            thumb: 'assets/img/DSC08914.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '57',
+            src: 'assets/img/DSC09789.jpg',
+            thumb: 'assets/img/DSC09789.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '58',
+            src: 'assets/img/DSC09851.jpg',
+            thumb: 'assets/img/DSC09851.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 1
+        },
+        {
+            id: '59',
+            src: 'assets/img/DSC09862.jpg',
+            thumb: 'assets/img/DSC09862.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '60',
+            src: 'assets/img/IMG_0219(1).jpg',
+            thumb: 'assets/img/IMG_0219(1).jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
+        },
+        {
+            id: '61',
+            src: 'assets/img/IMG_0436-4.jpg',
+            thumb: 'assets/img/IMG_0436-4.jpg',
+            format: 0,
+            subHtml: 'Image',
+            kindOfShooting: 2
         }
     ]
 }
