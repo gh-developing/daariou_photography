6dc16c5e0db0b92d9e597194af6ed1e7c5ed195d
Shachar Alon <shachar.alon@bystronic.com>
2022-06-20 11:42:48 +0200

    Holly molly guacamole fn deploy

--- Project/Frontend/daariiou-photography-frontend/src/app/components/q-and-a/q-and-a.component.ts
+++ Project/Frontend/daariiou-photography-frontend/src/app/components/q-and-a/q-and-a.component.ts
@@ -17,6 +17,7 @@ export class QAndAComponent implements OnInit {
     this.getQandAs();
   }
 
+<<<<<<< Updated upstream
   public getQandAs() {
     this.qandAService.apiV1QandAGetAllGet$Json().subscribe(
       (result) => {
@@ -28,4 +29,35 @@ export class QAndAComponent implements OnInit {
       }
     );
   }
+=======
+  public qanda1: any = [
+    {
+      question: "Seit wann fotografiere ich?",
+      answer: "Ich fotografiere seit 4 Jahren, seit einem Jahr fotografiere ich Professionel."
+    },
+    {
+      question: "Was und womit bearbeite ich an den Bildern?",
+      answer: "Ich bearbeite nur kleinere Sachen wie Pickel oder Falten. Ich mache aber keine Körperveränderungen. Die Bilder bearbeite ich auf Adobe Lightroom und Adobe Photoshop."
+    },
+    {
+      question: "Bei weiteren Fragen, kannst du mich jederzeit gerne über Instagram oder per Kontakt Formular kontaktieren.",
+      answer: ""
+    }
+  ]
+
+  public qanda: any = [
+    {
+      question: "Wie teuer ist ein Shooting?",
+      answer: "Ein Shooting kostet zwischen 50 und 250 Fr., je nach Aufwand, Länge und spezifischen wünschen."
+    },
+    {
+      question: "Wie langen brauchen die Bilder bis sie angekommen sind?",
+      answer: "Die Bilder kommen zwischen 5 Tagen und 2 Wochen fertig bearbeitet und in digitaler Form bei dir an."
+    },
+    {
+      question: "Mit welcher Kamera fotografiere ich?",
+      answer: "Ich fotografiere mit einer Sony A7RII, vorzugsweise mit einer Festbrennweite 80mm f1.8 von Viltrox oder dem 70-200mm f2.8 von Tamron."
+    }
+  ]
+>>>>>>> Stashed changes
 }
